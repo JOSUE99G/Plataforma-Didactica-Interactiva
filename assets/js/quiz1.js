a05527d283367fb9abad1a4fae67cc09c7db1639
JOSUE99G <guarandajosue99@gmail.com>
2026-06-29 19:54:02 -0500

    sistemas de evaluaciones

---
+++ assets/js/quiz1.js
@@ -0,0 +1,58 @@
+document.getElementById("quizForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let totalPreguntas = 3;
+    let respuestasCorrectas = 0;
+
+    // Obtener todas las respuestas seleccionadas
+    const respuestas = document.querySelectorAll('input[type="radio"]:checked');
+
+    // Verificar que todas las preguntas estén contestadas
+    if(respuestas.length < totalPreguntas){
+
+        alert("⚠ Debes responder todas las preguntas.");
+
+        return;
+    }
+
+    // Contar respuestas correctas
+    respuestas.forEach(respuesta =>{
+
+        respuestasCorrectas += Number(respuesta.value);
+
+    });
+
+    // Calcular nota sobre 100
+    let nota = Math.round((respuestasCorrectas / totalPreguntas) * 100);
+
+    // Mostrar resultado
+    let resultado = document.getElementById("resultado");
+
+    if(nota >= 70){
+
+        resultado.innerHTML = `
+            <h2 style="color:green;">
+                ✅ ¡Aprobaste!
+            </h2>
+
+            <p>Tu calificación es:</p>
+
+            <h1>${nota}/100</h1>
+        `;
+
+    }else{
+
+        resultado.innerHTML = `
+            <h2 style="color:red;">
+                ❌ No aprobaste
+            </h2>
+
+            <p>Tu calificación es:</p>
+
+            <h1>${nota}/100</h1>
+        `;
+
+    }
+
+});
